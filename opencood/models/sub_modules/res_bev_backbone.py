31b84836beb509732ec5c7105ad271430552b2fc
MinyiWan <minyiw@kth.se>
2026-03-18 15:08:27 +0100

    module compatibility change

--- opencood/models/sub_modules/res_bev_backbone.py
+++ opencood/models/sub_modules/res_bev_backbone.py
@@ -55,7 +55,7 @@ class ResBEVBackbone(nn.Module):
                         nn.ReLU()
                     ))
                 else:
-                    stride = np.round(1 / stride).astype(np.int)
+                    stride = np.round(1 / stride).astype(int)
                     self.deblocks.append(nn.Sequential(
                         nn.Conv2d(
                             num_filters[idx], num_upsample_filters[idx],
